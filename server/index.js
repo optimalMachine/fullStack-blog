23345e4299ca0912cd0d7bf0cb912a17fe72d0eb
Jiuk Choi <jiukchoi@algotradingsysteminc.com>
2022-08-01 21:04:10 -0500

    finish backend
    starts frontend

---
+++ server/index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+const mongoose = require('mongoose');
+const UserModel = require('./models/Users')
+const cors = require('cors');
+
+app.use(express.json());
+app.use(cors());
+
+mongoose.connect('mongodb+srv://admin:YH0XlYv8aAxcQ8ti@cluster0.lo1xk.mongodb.net/mern-tutorial?retryWrites=true&w=majority');
+
+app.get('/getUsers',(req,res)=>{
+    UserModel.find({}, (err,result)=>{
+        if(err){
+            res.json(err);
+        }else{
+            res.json(result);
+        }
+    })
+})
+
+app.post('/createUser', async (req,res)=>{
+    const user = req.body;
+    const newUser = new UserModel(user);
+    await newUser.save();
+
+    res.json(user);
+})
+
+app.listen(PORT,()=>{
+    console.log(`Server runs perfectly on ${PORT}`);
+});
+
+
+
